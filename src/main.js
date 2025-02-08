157ec98e332df60d4352ab4e149743670321710e
vrigue <vrielleguevarra@gmail.com>
2025-02-08 09:44:31 -0500

    basic structure

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import GameScene from './game.js'
+import EndingScene from './ending.js'
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1200,
+    height: 660,
+    backgroundColor: '#000000',
+    pixelArt: true,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene:
+        [
+        GameScene,
+        EndingScene
+    ],
+    audio: {
+        disableWebAudio: true
+    }
+};
+
+const game = new Phaser.Game(config);
